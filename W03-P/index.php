5cfe25f534de7e965a5ba8b33b906c5b86770067
Jiwon-01 <70565595+Jiwon-01@users.noreply.github.com>
2020-09-21 21:37:47 +0900

    Add files via upload

---
+++ W03-P/index.php
@@ -0,0 +1,51 @@
+<?php
+  $link = mysqli_connect('localhost', 'root', 'rootroot', 'dbp');
+  $query = "SELECT * FROM movie";
+  $result = mysqli_query($link, $query);
+  $list = '';
+  while ($row = mysqli_fetch_array($result)) {
+      $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  }
+
+  $article = array(
+    'title' => 'Welcome',
+    'description' => 'My Recommendation!'
+  );
+
+  $update_link = '';
+  $delete_link = '';
+
+  if (isset($_GET['id'])) {
+      $query = "SELECT * FROM movie WHERE id={$_GET['id']}";
+      $result = mysqli_query($link, $query);
+      $row = mysqli_fetch_array($result);
+      $article = array(
+      'title' => $row['title'],
+      'description' => $row['description']
+    );
+      $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
+      $delete_link = '
+        <form action="process_delete.php" method="POST">
+          <input type="hidden" name="id" value="'.$_GET['id'].'">
+          <input type="submit" value="delete">
+          </form>
+        ';
+  }
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> MOVIES </title>
+</head>
+<body>
+  <h1> <a href="index.php"> MOVIES </a></h1>
+  <ol> <?= $list ?> </ol>
+  <a href="create.php">create</a>
+  <?=$update_link?>
+  <?=$delete_link?>
+  <h2> <?= $article['title'] ?> </h2>
+  <?= $article['description'] ?>
+</body>
+</html>
