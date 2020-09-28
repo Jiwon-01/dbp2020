245183bcb670966b612c2dee4ac34d6f4507efeb
Jiwon-01 <70565595+Jiwon-01@users.noreply.github.com>
2020-09-28 11:41:31 +0900

    Add files via upload

---
+++ W04-P/director.php
@@ -0,0 +1,86 @@
+<?php
+$link = mysqli_connect('localhost', 'root', 'rootroot', 'dbp');
+
+$query = "SELECT * FROM director";
+$result = mysqli_query($link, $query);
+$director_info = '';
+
+while ($row = mysqli_fetch_array($result)) {
+    $filtered = array(
+    'id' => htmlspecialchars($row['id']),
+    'name' => htmlspecialchars($row['name']),
+    'profile' => htmlspecialchars($row['profile'])
+  );
+    $director_info .= '<tr>';
+    $director_info .= '<td>'.$filtered['id'].'</td>';
+    $director_info .= '<td>'.$filtered['name'].'</td>';
+    $director_info .= '<td>'.$filtered['profile'].'</td>';
+    $director_info .= '<td><a href="director.php?id='.$filtered['id'].'">update</a></td>';
+    $director_info .= '
+    <td>
+      <form action="process_delete_director.php" method="post">
+        <input type="hidden" name="id" value="'.$filtered['id'].'">
+        <input type="submit" value="delete">
+        </form>
+    </td>';
+    $director_info .= '</tr>';
+};
+
+$escaped = array(
+  'name' => '',
+  'profile' => ''
+);
+
+$form_action = 'process_create_director.php';
+$label_submit = 'Create director';
+$form_id = '';
+
+if (isset($_GET['id'])) {
+    $filtered_id = mysqli_real_escape_string($link, $_GET['id']);
+    settype($filtered_id, 'integer');
+    $query = "SELECT * FROM director WHERE id = {$filtered_id}";
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_array($result);
+    $escaped['name'] = htmlspecialchars($row['name']);
+    $escaped['profile'] = htmlspecialchars($row['profile']);
+
+    $form_action = 'process_update_director.php';
+    $label_submit = 'Update director';
+    $form_id = '<input type="hidden" name="id" value="'.$_GET['id'].'">';
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset = "utf-8">
+  <title>MOVIES</title>
+</head>
+<body>
+  <h1><a href="index.php">MOVIES</a></h1>
+  <p><a href="index.php">movie</a></p>
+
+  <table border="1">
+    <tr>
+      <th>id</th>
+      <th>name</th>
+      <th>profile</th>
+      <th>update</th>
+      <th>delete</th>
+    </tr>
+    <?= $director_info ?>
+  </table>
+  <br>
+    <form action="<?= $form_action ?>" method="post">
+      <?= $form_id ?>
+    <label for="fname">name:</label><br>
+    <input type="text" id="name" name="name" placeholder="name"
+     value="<?=$escaped['name']?>"><br>
+    <label for="lname">profile:</label><br>
+    <input type="text" id="profile" name="profile" placeholder="profile"
+    value="<?=$escaped['profile']?>"><br><br>
+    <input type="submit" value="<?= $label_submit ?>">
+  </form>
+</body>
+</html>
---
+++ W04-P/process_create.php
@@ -0,0 +1,25 @@
+<?php
+  $link = mysqli_connect('localhost', 'root', 'rootroot', 'dbp');
+  $filtered = array(
+    'title' => mysqli_real_escape_string($link, $_POST['title']),
+    'description' => mysqli_real_escape_string($link, $_POST['description']),
+    'director_id' => mysqli_real_escape_string($link, $_POST['director_id'])
+  );
+  $query = "
+    INSERT INTO movie (
+      title, description, created, director_id)
+      VALUES (
+        '{$filtered['title']}',
+        '{$filtered['description']}',
+        now(),
+        '{$filtered['director_id']}'
+        )
+  ";
+
+  $result = mysqli_multi_query($link, $query);
+  if ($result == false) {
+      echo '저장하는 과정에서 문제가 발생했습니다. 관리자에게 문의하세요.';
+      error_log(mysqli_error($link));
+  } else {
+      echo '성공했습니다. <a href="index.php">돌아가기</a>';
+  }
